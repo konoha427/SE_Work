dd1d94de9f131b1d7a54af90195228fb55a3415a
konoha427 <swh_ustc@126.com>
2014-09-16 12:57:52 +0800

    Format_regularity

--- menu.c
+++ menu.c
@@ -33,78 +33,78 @@ int Help();
 
 typedef struct DataNode
 {
-	char *cmd;
-	char *desc;
-	int  (*handler)();
-	struct DataNode *next;
+    char *cmd;
+    char *desc;
+    int  (*handler)();
+    struct DataNode *next;
 }tDataNode;
 
 tDataNode * FindCmd(tDataNode * head,char * cmd)
 {
-	if (head == NULL || cmd == NULL)
-	{
-		return NULL;
-	}
-	tDataNode * p = head;
-	while (p != NULL)
-	{
-		if (strcmp(p->cmd, cmd) == 0)
-		{
-			return p;
-			break;
-		}
-		p = p->next;
-	}
-	return NULL;
+    if (head == NULL || cmd == NULL)
+    {
+        return NULL;
+    }
+    tDataNode * p = head;
+    while (p != NULL)
+    {
+        if (strcmp(p->cmd, cmd) == 0)
+        {
+            return p;
+            break;
+        }
+        p = p->next;
+    }
+    return NULL;
 }
 
 int ShowAllCmd(tDataNode * head)
 {
-	printf("Menu List:\n");
-	tDataNode * p = head;
-	while (p != NULL)
-	{
-		printf("%s - %s\n", p->cmd, p->desc);
-		p = p->next;
-	}
-	return 0;
+    printf("Menu List:\n");
+    tDataNode * p = head;
+    while (p != NULL)
+    {
+        printf("%s - %s\n", p->cmd, p->desc);
+        p = p->next;
+    }
+    return 0;
 }
 
 /* menu program */
 
 static tDataNode head[] =
 {
-	{ "help", "this is help command!", Help, &head[1] },
-	{ "version", "menu program v1.1", NULL, &head[2] },
-	{ "list", "this is list command", NULL, NULL }
+    { "help", "this is help command!", Help, &head[1] },
+    { "version", "menu program v1.1", NULL, &head[2] },
+    { "list", "this is list command", NULL, NULL }
 };
 
 int main()
 {
-	/* cmd line begins */
-	while (1)
-	{
-		char cmd[CMD_MAX_LEN];
-		printf("Input a Command > ");
-		scanf("%s",cmd);
-		tDataNode *p = FindCmd(head,cmd);
-		if (p == NULL)
-		{
-			printf("This is a wrong Conmmand!\n");
-			continue;
-		}
-		printf("%s - %s\n", p->cmd, p->desc);
-		if (p->handler == Help)
-		{
-			p->handler();
-		}
-	}
+    /* cmd line begins */
+    while (1)
+    {
+        char cmd[CMD_MAX_LEN];
+        printf("Input a Command > ");
+        scanf("%s",cmd);
+        tDataNode *p = FindCmd(head,cmd);
+        if (p == NULL)
+        {
+            printf("This is a wrong Conmmand!\n");
+            continue;
+        }
+        printf("%s - %s\n", p->cmd, p->desc);
+        if (p->handler == Help)
+        {
+            p->handler();
+        }
+    }
 }
 
 int Help()
 {
-	ShowAllCmd(head);
-	return 0;
+    ShowAllCmd(head);
+    return 0;
 }
 
 
