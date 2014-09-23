5ebe10095493abb3d2c0818da51274b4c5dcf7ce
konoha427 <swh_ustc@126.com>
2014-09-23 19:40:05 +0800

    Revert "SE_Work2"
    This reverts commit e9f111590b6d9c052971169ffe802e5332e1ba2c.

--- menu.c
+++ menu.c
@@ -7,14 +7,14 @@
 /*  MODULE NAME           :  menu                                                                 */
 /*  LANGUAGE              :  C                                                                    */
 /*  TARGET ENVIRONMENT    :  ANY                                                                  */
-/*  DATE OF FIRST RELEASE :  2014/09/20                                                           */
+/*  DATE OF FIRST RELEASE :  2014/09/13                                                           */
 /*  DESCRIPTION           :  This is a menu program                                               */
 /**************************************************************************************************/
 
 /*
 * Revision log:
 *
-* Created by Shi Weihao JG14225071, 2014/09/20
+* Created by Shi Weihao JG14225071, 2014/09/13
 *
 */
 
@@ -22,128 +22,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "menu.h"
 
 #define DESC_LEN    1024
 #define CMD_NUM     10
 #define CMD_MAX_LEN 1024
 
-/*initialize globle variable*/
-tLinkTable * pLinkHead = NULL;
-int count = 5;
+int Help();
 
-/*find the command given*/
-tDataNode * FindCmd(tLinkTable * lhead, char * cmd)
+/* data struct and its operations */
+
+typedef struct DataNode
 {
-    tDataNode * pNode = (tDataNode*)GetLinkTableHead(lhead);
-    while (pNode != NULL)
+    char *cmd;
+    char *desc;
+    int  (*handler)();
+    struct DataNode *next;
+}tDataNode;
+
+tDataNode * FindCmd(tDataNode * head,char * cmd)
+{
+    if (head == NULL || cmd == NULL)
     {
-        if (!strcmp(pNode->cmd, cmd))
+        return NULL;
+    }
+    tDataNode * p = head;
+    while (p != NULL)
+    {
+        if (strcmp(p->cmd, cmd) == 0)
         {
-            return pNode;
+            return p;
+            break;
         }
-        pNode = (tDataNode*)GetNextLinkTableNode(lhead, (tLinkTableNode*)pNode);
+        p = p->next;
     }
     return NULL;
 }
 
-/*show all the command*/
-int ShowAllCmd(tLinkTable * lhead)
+int ShowAllCmd(tDataNode * head)
 {
-    tDataNode * pNode = (tDataNode*)GetLinkTableHead(lhead);
-    while (pNode != NULL)
+    printf("Menu List:\n");
+    tDataNode * p = head;
+    while (p != NULL)
     {
-        printf("%s - %s\n", pNode->cmd, pNode->desc);
-        pNode = (tDataNode*)GetNextLinkTableNode(lhead, (tLinkTableNode*)pNode);
+        printf("%s - %s\n", p->cmd, p->desc);
+        p = p->next;
     }
     return 0;
 }
 
-int Help()
-{
-    ShowAllCmd(pLinkHead);
-    return 0;
-}
+/* menu program */
 
-/*create the menu list*/
-int CreateMenu(tDataNode data[])
+static tDataNode head[] =
 {
-    pLinkHead = CreateLinkTable();
-    int i = 0;
-    while (i < count)
-    {
-        AddLinkTableNode(pLinkHead, (tLinkTableNode*)&data[i]);
-        i++;
-    }
-    return 0;
-}
+    { "help", "this is help command!", Help, &head[1] },
+    { "version", "menu program v1.1", NULL, &head[2] },
+    { "list", "this is list command", NULL, NULL }
+};
 
-/*add the command given to the menu list*/
-int AddCmd()
+int main()
 {
-    count = count + 1;
-    char AddCmd[CMD_MAX_LEN];
-    char AddDesc[DESC_LEN];
-    //while (getchar() != '\n');
-    printf("Add your CMD here > ");
-    scanf("%s",AddCmd);
-    printf("Add the Description here > ");
-    scanf("%s",AddDesc);
-    tDataNode * p = FindCmd(pLinkHead, AddCmd);
-    if (p == NULL)
-    {
-        strcpy(data[count - 1].cmd, AddCmd);
-        strcpy(data[count - 1].desc, AddDesc);
-        AddLinkTableNode(pLinkHead, (tLinkTableNode*)&data[count - 1]);
-        printf("CMD add success!\n");
-    }
-    else
-    {
-        printf("The CMD has already exists!\n");
-    }
-    return 0;
-}
-
-/*delete the command given from the list*/
-int DelCmd()
-{
-    char DelCmd[CMD_MAX_LEN];
-    printf("Input the CMD you want to delete here > ");
-    scanf("%s", DelCmd);
-    tDataNode * pFind = FindCmd(pLinkHead, DelCmd);
-    if (pFind == NULL)
-    {
-        printf("There is no CMD in list!\n");
-    }
-    else if (!strcmp(pFind->cmd, "AddCmd"))
-    {
-        printf("This CMD is System CMD,You can NOT DELETE it!\n");
-    }
-    else if (!strcmp(pFind->cmd, "DelCmd"))
-    {
-        printf("This CMD is System CMD,You can NOT DELETE it!\n");
-    }
-    else if (!strcmp(pFind->cmd, "help"))
-    {
-        printf("This CMD is System CMD,You can NOT DELETE it!\n");
-    }
-    else
-    {
-        DelLinkTableNode(pLinkHead, (tLinkTableNode*)pFind);
-        printf("The CMD has been DELETED!\n");
-    }
-    return 0;
-}
-
-/*run the menu list*/
-int RunMenu()
-{
+    /* cmd line begins */
     while (1)
     {
         char cmd[CMD_MAX_LEN];
         printf("Input a Command > ");
-        scanf("%s", cmd);
-        tDataNode * p = FindCmd(pLinkHead, cmd);
+        scanf("%s",cmd);
+        tDataNode *p = FindCmd(head,cmd);
         if (p == NULL)
         {
             printf("This is a wrong Conmmand!\n");
@@ -154,14 +98,30 @@ int RunMenu()
         {
             p->handler();
         }
-        if (p->handler == AddCmd)
-        {
-            p->handler();
-        }
-        if (p->handler == DelCmd)
-        {
-            p->handler();
-        }
     }
+}
+
+int Help()
+{
+    ShowAllCmd(head);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
